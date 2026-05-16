46f2abce14a834a90e6c588c791394ded39a9051
lallen-student <lallendelice@umass.edu>
2026-05-16 07:39:42 -0400

    edits

--- codesForDrafts.R
+++ codesForDrafts.R
@@ -17,28 +17,37 @@ str(mtcars)
 
 # deliverable 1 ----------------------------------------------------------
 
-gearCounts <- mtcars |>
+del1Data <- mtcars |> 
   count(gear)
 
-del1Draft <- ggplot(gearCounts, aes(
-    x = n,
-    y = factor(gear))) +
+del1Draft <- ggplot(del1Data, aes(
+  x = n,
+  y = factor(gear))) +
   
   geom_col(fill = "lightgray") +
   
   geom_text(aes(label = n),
-    color = "blue",
-    size = 8,
-    hjust = 1.3
+            color = "blue",
+            size = 8,
+            hjust = 1.3
   ) +
   
   labs(
     title = "Count of Cars per GEAR",
+    caption = "Source: mtcars data",
     x = "",
     y = "Gear"
   ) +
   
-  theme_minimal()
+  scale_x_continuous(
+    expand = expansion(mult = c(0, .15))
+  ) +
+  
+  theme_minimal() +
+  
+  theme(
+    plot.margin = margin(10, 20, 10, 10)
+  )
 
 del1Draft
 
@@ -48,11 +57,11 @@ saveRDS(del1Draft, file = "del1Draft.rds")
 
 # deliverable 2 ----------------------------------------------------------
 
-carbCounts <- mtcars %>%
+del2Data <- mtcars |>
   count(carb)
 
 del2Draft <- ggplot(
-  carbCounts,
+  del2Data,
   aes(
     x = factor(carb),
     y = n
@@ -70,18 +79,21 @@ del2Draft <- ggplot(
   
   labs(
     title = "Count of Cars per CARB",
+    caption = "Source: mtcars data",
     x = "CARB",
     y = ""
   ) +
-  
-  theme_minimal()
+  theme(
+    plot.margin = margin(20, 10, 10, 10)
+  ) +
+  theme_minimal() +
+  scale_y_continuous(expand = expansion(mult = c(0, .15)))
 
 del2Draft
 
-
 # save del2Draft ----------------------------------------------------------
-saveRDS(del2Draft, file = "del2Draft.rds")
 
+saveRDS(del2Draft, file = "del2Draft.rds")
 
 # deliverable 3 ----------------------------------------------------------
 
@@ -110,11 +122,20 @@ del3Draft <- ggplot(del3Data,
   
   labs(
     title = "Only 5-gear cars utilize the 6 and 8 carburetor setups.",
+    caption = "Source: mtcars data",
     x = "cylinders",
     y = "Count"
   ) +
-  
-  theme_minimal()
+  theme_minimal() +
+  theme(
+    plot.margin = margin(20, 10, 10, 10)
+  ) +
+  theme(plot.caption = element_text(
+      hjust = 1,
+      size = 8,
+      color = "gray40"
+    )) +
+      scale_y_continuous(expand = expansion(mult = c(0, .15)))
 
 del3Draft
 
